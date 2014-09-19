ad2a2be495371dc55229b8d182adbeee6873ca8d
Agnaldo Luiz <agnaldoluiz@gmail.com>
2014-09-18 21:30:59 -0500

    Hello World JAVA - Beginning of Pinger project

---
+++ src/Pinger.java
@@ -0,0 +1,10 @@
+/**
+ * Created by agnaldocunha on 9/18/14.
+ * @author agnaldocunha
+ */
+public class Pinger {
+
+    public static void main(String [] args) {
+        System.out.print("Hello World");
+    }
+}
